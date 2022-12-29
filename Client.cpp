d46c85de729147c0a23baee08e7531bacc44ffe7
awesome0597 <adira.weiss@gmail.com>
2022-12-29 19:24:25 +0200

    added input checks file for client side added errors and exits removed unnecessary lines of code

--- Client.cpp
+++ Client.cpp
@@ -7,11 +7,18 @@
 #include <unistd.h>
 #include <cstring>
 #include <string>
-#include "SafetyChecks.h"
+#include "ClientInputCheck.h"
+
 
 
 int main(int argc, char **argv) {
-    const char* ip = argv[1];
+    // check that argc is 3
+    // check that argv[1] is a valid IP address
+    // check that argv[2] is a valid port number
+    if (!inputCheck(argc, argv)) {
+        std::perror("Invalid input");
+    }
+    const char *ip = argv[1];
     const int port = atoi(argv[2]);
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket < 0) {
@@ -25,19 +32,19 @@ int main(int argc, char **argv) {
     if (connect(client_socket, (struct sockaddr *) &remote_address, sizeof(remote_address)) < 0) {
         std::perror("error connecting to server");
     }
-    //flag for while loop
-    bool run = true;
-    //received input from user
-    std::string input;
-    std::getline(std::cin, input);
-    while (run) {
-        // declaring character array (+1 for null terminator)
-        char* data_addr = new char[input.length() + 1];
-        // copying the contents of the input to char array to be sent to server
-        std::strcpy(data_addr, input.c_str());
-        int data_len = strlen(data_addr);
+
+
+    while (true) {
+        //received input from user
+        std::string input;
+        std::getline(std::cin, input);
+        //check if we need to close the connection
+        if (input == "-1") {
+            break;
+        }
+        int data_len = input.size() + 1;
         // sending data to the server
-        int sent_bytes = send(client_socket, data_addr, data_len, 0);
+        int sent_bytes = send(client_socket, input.c_str(), data_len, 0);
         if (sent_bytes < 0) {
             std::perror("error sending message");
         }
@@ -46,24 +53,15 @@ int main(int argc, char **argv) {
         int read_bytes = recv(client_socket, buffer, expected_data_len, 0);
         if (read_bytes < 0) {
             std::perror("error reading from socket");
-        }else if (read_bytes == 0) {
+        } else if (read_bytes == 0) {
             // if connection to server is closed we end the program
-            std::cout << "server disconnected" << std::endl;
-            run = false;
-        }else {
+            std::perror("server disconnected");
+        } else {
             std::cout << buffer << std::endl;
         }
-        //prepare to receive new input from user
-        input.clear();
-        delete[] data_addr;
-        std::getline(std::cin, input);
-        if (input == "-1") {
-            run = false;
-        }
+
     }
     close(client_socket);
 
     return 0;
-
-
 }
---
+++ ClientInputCheck.h
@@ -0,0 +1,15 @@
+//
+// Created by lenovo on 12/29/2022.
+//
+
+#ifndef UNTITLED_CLIENTINPUTCHECK_H
+#define UNTITLED_CLIENTINPUTCHECK_H
+
+bool isValidPort(std::string port);
+
+bool isValidIP(std::string ip);
+
+bool inputCheck(int argc, char **argv);
+
+
+#endif //UNTITLED_CLIENTINPUTCHECK_H
